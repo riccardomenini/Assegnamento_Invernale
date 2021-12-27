e12514c6fa7a523e772b6348c26c4517358c4cc4
Riccardo Menini <riccardo.menini-1@studenti.unitn.it>
2021-12-27 23:46:50 +0100

    aggiunta del calcolo del tempo e primi passi per implementazione meccanismo RW

--- manipolazionedeipezzidapartedeicobot.cpp
+++ manipolazionedeipezzidapartedeicobot.cpp
@@ -2,7 +2,85 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
+#include <mutex>
+#include "manipolazionedeipezzidapartedeicobot.h"
 using namespace std;
+
+/*RW::RW()
+:data_{0}, readers_{0}, writers_{0}
+{}
+
+// operation StartRead
+//	if writers != 0 or not empty (OKtoWrite)
+//		waitC(OKtoRead)
+//	readers = readers + 1
+//	signalC(OKtoRead)
+void RW::start_read()
+{
+	unique_lock<mutex> mlock(mutex_);
+	// must use while for spourious wake ups
+	while ((writers_ != 0) || (waiting_writers_ > 0)) {
+		waiting_readers_++;
+		OK_to_read_.wait(mlock);
+		waiting_readers_--;
+		// if writers_==0 the reader was unblocked by
+		// a notify_all() and need to get out of the while
+		// without checking the waiting_writers_>0
+		// otherwise readers get stacked till all the writers
+		// are gone
+		if (writers_ == 0)
+			break;
+	}
+	readers_++;
+}
+
+
+//operation EndRead
+//	readers = readers - 1
+//	if readers = 0
+//		signalC(OKtoWrite)
+void RW::end_read()
+{
+	unique_lock<mutex> mlock(mutex_);
+	readers_--;
+	if (readers_ == 0)
+		OK_to_write_.notify_one();
+
+}
+
+
+//operation StartWrite
+//	if writers != 0 or readers != 0
+//		waitC(OKtoWrite)
+//	writers = writers + 1
+void RW::start_write()
+{
+	unique_lock<mutex> mlock(mutex_);
+	while ( (writers_ != 0) || (readers_ != 0) ) {
+		waiting_writers_++;
+		OK_to_write_.wait(mlock);
+		waiting_writers_--;
+	}
+	writers_++;
+}
+
+
+//operation EndWrite
+//	writers = writers - 1
+//	if (empty(OKtoRead))
+//	then signalC(OKtoWrite)
+//	else signalC(OKtoRead)
+void RW::end_write()
+{
+	unique_lock<mutex> mlock(mutex_);
+	writers_--;
+	if (waiting_readers_ > 0)
+		OK_to_read_.notify_all();
+	else
+		OK_to_write_.notify_one();
+}
+*/
 
 void manipolazionedeipezzidapartedeicobot(const vector<int> & veldis, string nome){
 	ifstream myfile("arrivi_linea_"+ nome +".txt");
@@ -14,18 +92,28 @@ void manipolazionedeipezzidapartedeicobot(const vector<int> & veldis, string nom
 	string a_line;
 	while (getline (myfile, a_line))
 		lines.push_back(a_line);
-	myfile.close();
+
   for (int i = 0; i < lines.size(); i++){
     size_t found1 = lines[i].find_first_of("ABC");
-    size_t found2 = lines[i].find_first_of("123456789",found1);
+    size_t found2 = lines[i].find_first_of("0123456789",found1);
+    size_t found3 = lines[i].find_first_of("0123456789");
+    size_t found4 = lines[i].find_first_not_of("0123456789");
+    size_t found5 = lines[i].find_first_of("0123456789",found4);
+    size_t found6 = lines[i].find_first_not_of("0123456789",found5) - 2;
+    int mm = stoi(lines[i].substr(found3, found4));
+    int ss = stoi(lines[i].substr(found5, found6));
     string pos;
-    if(found2 != string::npos){
-      pos = lines[i].substr(found2, string::npos);
+    pos = lines[i].substr(found2, string::npos);
+    if (nome == "a"){
+      mm += floor((veldis[0]/veldis[1])/60.);
+      ss += (veldis[0]/veldis[1])%60;
     }else{
-      pos = lines[i][string::npos-1];
+      mm += floor((veldis[2]/veldis[3])/60.);
+      ss += (veldis[2]/veldis[3])%60;
     }
-    cout << "Cobot linea di trasporto " << nome << ": recuperato componente di tipo " << lines[i][found1] << " al tempo MM SS in posizione " << pos << '\n';
+    cout << "Cobot linea di trasporto " << nome << ": recuperato componente di tipo " << lines[i][found1] << " al tempo " << mm << " " << ss << " in posizione " << pos << '\n';
   }
+  myfile.close();
 
 
 
--- manipolazionedeipezzidapartedeicobot.h
+++ manipolazionedeipezzidapartedeicobot.h
@@ -2,6 +2,27 @@
 #define MANIPOLAZIONEDEIPEZZIDAPARTEDEICOBOT
 using namespace std;
 
+/*class RW {
+public:
+	RW();
+	void start_read();
+	void end_read();
+	void start_write();
+	void end_write();
+	void incr_data() { data_++; }
+	int get_data() { return data_;}
+
+private:
+	int data_;
+	int readers_;
+	int writers_;
+	int waiting_writers_;
+	int waiting_readers_;
+	std::mutex mutex_;   // RW monitor lock
+	std::condition_variable OK_to_read_;
+	std::condition_variable OK_to_write_;
+};*/
+
 void manipolazionedeipezzidapartedeicobot(const vector<int> &, string);
 
 
